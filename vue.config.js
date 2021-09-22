b6a7cb1673aa15211335e2754493fb23ed83420e
dpy.cn <468264345@qq.com>
2021-09-22 17:42:29 +0800

    配置vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+//最新版本copy-webpack-plugin插件暂不兼容，推荐v5.0.0
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+	configureWebpack: {
+		plugins: [
+			new CopyWebpackPlugin([{
+				from: path.join(__dirname, 'src/images'),
+				to: path.join(__dirname, 'dist', process.env.NODE_ENV === 'production' ? 'build' :
+					'dev',
+					process.env.UNI_PLATFORM, 'images')
+			}])
+		]
+	}
+}
