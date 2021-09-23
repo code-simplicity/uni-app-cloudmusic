11fa14b02b01874638b3e900feaeca5d4f7e0066
dpy <468264345@qq.com>
2021-09-23 18:01:13 +0800

    首页页面

---
+++ src/pages/home/index.vue
@@ -0,0 +1,24 @@
+
+<template>
+	
+</template>
+ 
+<script>
+	export default {
+		name: '',
+		data(){
+			return {
+				
+			}
+		},
+		component: {
+		
+		},
+		methods:{
+			
+		}
+	}
+</script>
+ 
+<style lang="scss" scoped>
+</style>
