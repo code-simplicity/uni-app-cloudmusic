8cdda959fc6a76b5e1ff4b89111eed66b4a6be51
dpy <468264345@qq.com>
2021-09-23 22:22:52 +0800

    使用uView组件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
+// 引入uView
+import uView from "uview-ui";
+Vue.use(uView);
 import App from './App'
+
+
+
 
 // 关闭生产提示
 Vue.config.productionTip = false
+
 
 App.mpType = 'app'
 
