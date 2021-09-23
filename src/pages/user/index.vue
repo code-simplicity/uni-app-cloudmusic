a91b8f9a710efe1c9dac24ded340299e0bb4970f
dpy <468264345@qq.com>
2021-09-23 22:23:25 +0800

    用户页面

---
+++ src/pages/user/index.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class=""></view>
+</template>
+
+<script>
+/**
+ * author	bugdr
+ * time     2021-9-23 9:52:09 ?F10: PM?
+ * description
+ */
+
+export default {
+	name: '',
+	data() {
+		return {};
+	},
+	component: {},
+	mounted() {},
+	methods: {}
+};
+</script>
+
+<style lang="scss" scoped></style>
