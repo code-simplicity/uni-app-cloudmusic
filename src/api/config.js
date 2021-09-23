b3833d7ed39c471d12feec0f1c12cd1e417a5e1c
dpy <468264345@qq.com>
2021-09-23 16:29:21 +0800

    配置接口请求地址

---
+++ src/api/config.js
@@ -0,0 +1,14 @@
+// 接口地址
+let api_base_url = ''
+if (process.env.NODE_ENV === 'development') {
+	// 开发地址
+	api_base_url = 'http://localhost:3000'
+} else if (process.env.NODE_ENV === 'production') {
+	// 生产模式的地址
+	api_base_url = 'https://netease-cloud-music-api-zeta-murex.vercel.app'
+}
+
+// 接口地址统一暴露
+export default {
+	api_base_url
+}
