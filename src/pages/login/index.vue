77fcb1788a5917345407e9a7bc8637eec4fa23a2
dpy <468264345@qq.com>
2021-09-23 18:01:44 +0800

    登录页面

---
+++ src/pages/login/index.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class="login-wrap"></view>
+</template>
+
+<script>
+/**
+ * author	bugdr
+ * time     2021-9-23 5:33:19 ?F10: PM?
+ * description
+ */
+
+export default {
+	name: '',
+	data() {
+		return {};
+	},
+	component: {},
+	mounted() {},
+	methods: {}
+};
+</script>
+
+<style lang="scss" scoped></style>
