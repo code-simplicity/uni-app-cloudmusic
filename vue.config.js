ebe8c9f83be960fca25e8babdebbe44d73348ab3
Romantic <468264345@qq.com>
2021-09-24 21:59:12 +0800

    配置路由导航插件

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,10 @@ const path = require('path')
 //最新版本copy-webpack-plugin插件暂不兼容，推荐v5.0.0
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
+const TransformPages = require('uni-read-pages')
+const {
+	webpack
+} = new TransformPages()
 module.exports = {
 	configureWebpack: {
 		plugins: [
@@ -10,7 +14,16 @@ module.exports = {
 				to: path.join(__dirname, 'dist', process.env.NODE_ENV === 'production' ? 'build' :
 					'dev',
 					process.env.UNI_PLATFORM, 'images')
-			}])
+			}]),
+			// 配置uni-read-pages路由
+			new webpack.DefinePlugin({
+				ROUTES: webpack.DefinePlugin.runtimeValue(() => {
+					const tfPages = new TransformPages({
+						includes: ['path', 'name', 'aliasPath']
+					});
+					return JSON.stringify(tfPages.routes)
+				}, true)
+			})
 		]
 	},
 	// 配置网络请求
