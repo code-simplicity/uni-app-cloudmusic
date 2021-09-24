a759c91ebc08725246adc5a2480246556b2d0d52
Romantic <468264345@qq.com>
2021-09-24 21:53:48 +0800

    直播

---
+++ src/pages/live/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="">
+		直播
+	</view>
+</template>
+
+<script>
+/**
+ * author	bugdr
+ * time     2021-9-24 9:31:07 ?F10: PM?
+ * description
+ */
+
+export default {
+	name: '',
+	data() {
+		return {};
+	},
+	component: {},
+	mounted() {},
+	methods: {}
+};
+</script>
+
+<style lang="scss" scoped></style>
