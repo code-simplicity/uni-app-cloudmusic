417a088656828bcdd0c9298b1fc4a693cc16b697
Romantic <468264345@qq.com>
2021-09-24 21:52:58 +0800

    关注页面

---
+++ src/pages/focus/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="">
+		关注
+	</view>
+</template>
+
+<script>
+/**
+ * author	bugdr
+ * time     2021-9-24 9:31:41 ?F10: PM?
+ * description
+ */
+
+export default {
+	name: '',
+	data() {
+		return {};
+	},
+	component: {},
+	mounted() {},
+	methods: {}
+};
+</script>
+
+<style lang="scss" scoped></style>
