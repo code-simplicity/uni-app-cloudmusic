885b926954cf9c622dfd1369c4564f4589aaeca1
Romantic <468264345@qq.com>
2021-09-24 21:54:44 +0800

    广场页面

---
+++ src/pages/square/index.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class="">广场</view>
+</template>
+
+<script>
+/**
+ * author	bugdr
+ * time     2021-9-24 9:32:38 ?F10: PM?
+ * description
+ */
+
+export default {
+	name: '',
+	data() {
+		return {};
+	},
+	component: {},
+	mounted() {},
+	methods: {}
+};
+</script>
+
+<style lang="scss" scoped></style>
