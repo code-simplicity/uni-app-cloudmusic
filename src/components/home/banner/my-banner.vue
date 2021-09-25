31879f8950363a4a175bcc29783668043b89a1bc
Romantic <468264345@qq.com>
2021-09-25 20:42:52 +0800

    轮播图的功能开发

---
+++ src/components/home/banner/my-banner.vue
@@ -0,0 +1,44 @@
+<template>
+	<view class="my-banner"><u-swiper :list="bannerList" :interval="4000" name="pic"></u-swiper></view>
+</template>
+
+<script>
+/**
+ * author	bugdr
+ * time     2021-9-25 9:25:32 ?F10: AM?
+ * description
+ */
+
+export default {
+	name: 'my-banner',
+	data() {
+		return {
+			bannerList: [],
+			// 获取轮播图类型,1为安卓
+			type: 1
+		};
+	},
+	component: {},
+	mounted() {
+		this.getBanner();
+	},
+	methods: {
+		// 获取轮播图
+		getBanner() {
+			this.$api.getBanner(this.type).then(res => {
+				if (res.code === this.$code.code_status) {
+					this.bannerList = res.banners;
+				}
+			});
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.my-banner {
+	width: 100%;
+	height: 50rpx;
+	margin-bottom: 20rpx;
+}
+</style>
