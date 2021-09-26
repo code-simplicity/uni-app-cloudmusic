9c3006f772b2addbd4ebdd665458ab270de88cdb
Romantic <468264345@qq.com>
2021-09-26 22:29:19 +0800

    暴露歌曲接口

--- src/api/index.js
+++ src/api/index.js
@@ -3,10 +3,12 @@
 import * as user from './service/user.js'
 import * as banner from './service/banner.js'
 import * as songlist from './service/songlist.js'
+import * as song from './service/song.js'
 
 // 统一暴露
 export default {
 	...user,
 	...banner,
-	...songlist
+	...songlist,
+	...song,
 }
