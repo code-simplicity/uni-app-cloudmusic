a0e426a55a50103515831e4cb9c47837b7e421e7
Romantic <468264345@qq.com>
2021-09-26 22:28:50 +0800

    开发环境地址的改变

--- src/api/config.js
+++ src/api/config.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === 'development') {
 	api_base_url = 'http://localhost:3000'
 } else if (process.env.NODE_ENV === 'production') {
 	// 生产模式的地址
-	api_base_url = 'https://netease-cloud-music-api-zeta-murex.vercel.app'
+	api_base_url = 'https://netease-cloud-music-api-zeta-murex.vercel.app/'
 }
 
 // 接口地址统一暴露
