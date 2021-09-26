26693ea26872c86607c44f89e0b3b27a8bf38054
Romantic <468264345@qq.com>
2021-09-26 22:34:37 +0800

    暴露状态管理

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import createLogger from 'vuex/dist/logger'
 import user from './modules/user.js'
+import player from './modules/player.js'
 // 使用vuex
 Vue.use(Vuex)
 
@@ -10,7 +11,7 @@ const debug = process.env.NODE_ENV !== 'production'
 
 export default new Vuex.Store({
     modules: {
-        // player,
+        player,
         // search,
         // singer,
         user,
