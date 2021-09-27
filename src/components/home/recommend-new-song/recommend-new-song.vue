bd68d40b84e9b5d9a0ce1f2284607f24f01363db
Romantic <468264345@qq.com>
2021-09-27 22:27:42 +0800

    新歌推荐完成图片上出现动画

--- src/components/home/recommend-new-song/recommend-new-song.vue
+++ src/components/home/recommend-new-song/recommend-new-song.vue
@@ -12,35 +12,36 @@
 							class="wrapper flex-center"
 							v-for="(item, index) in newSong.slice(column * 3, column * 3 + 3)"
 							:key="item.id"
+							:class="index === currentIndex && currentSong.id == item.id && playing ? 'playing' : ''"
 						>
 							<view class="index-container flex-center">
-								<view
-									class="cover"
-									:class="
-										index === currentIndex && currentSong.id === item.id && playing ? 'playing' : ''
-									"
-								>
-									<u-image
-										mode="widthFix"
+								<view class="cover">
+									<image
+										style="width: 100rpx; height: 100rpx;"
+										mode="aspectFit"
 										border-radius="10"
 										:src="item.image + '?param=270y270'"
-									></u-image>
+									></image>
 								</view>
 								<view class="play-icon">
-									<div class="line" style="animation-delay: -1.2s"></div>
-									<div class="line" style="animation-delay: -1s"></div>
-									<div class="line" style="animation-delay: -0.8s"></div>
-									<div class="line" style="animation-delay: -0.6s"></div>
-									<div class="line" style="animation-delay: -0.4s"></div>
+									<view class="line animate__animated animate__bounce animate__delay-5s 5s"></view>
+									<view class="line animate__animated animate__bounce animate__delay-4s 4s"></view>
+									<view class="line animate__animated animate__bounce animate__delay-3s 3s"></view>
+									<view class="line animate__animated animate__bounce animate__delay-2s 2s"></view>
+									<view class="line animate__animated animate__bounce animate__delay-1s 1s"></view>
 								</view>
 								<u-icon
 									class="play-btn"
-									custom-prefix="my-icon"
-									name="bofang1"
-									size="64"
+									custom-prefix="iconfont"
+									name="iconfont icon-bofang1"
 									@click="playMusci(item, index)"
 								></u-icon>
-								<u-icon class="pause-btn" name="pause-circle" size="36" @click="pauseMusci"></u-icon>
+								<u-icon
+									class="pause-btn"
+									custom-prefix="iconfont"
+									name="iconfont icon-zanting1"
+									@click="pauseMusci"
+								></u-icon>
 							</view>
 							<view class="song-info">
 								<view class="song-name">
@@ -83,6 +84,11 @@ export default {
 	component: {},
 	mounted() {},
 	methods: {
+		// 暂停
+		pauseMusci() {
+			this.pausePlay();
+		},
+
 		// 播放音乐
 		playMusci(item, index) {
 			this.selectPlay({
@@ -130,13 +136,13 @@ export default {
 				margin-bottom: 10rpx;
 				.wrapper {
 					width: 100%;
-					justify-content: start;
 					position: relative;
 					margin-bottom: 40rpx;
 					.index-container {
 						margin-right: 26rpx;
 						position: relative;
-						margin-bottom: 20rpx;
+						margin-bottom: 30rpx;
+
 						.cover {
 							width: 100rpx;
 							height: 100rpx;
@@ -144,30 +150,39 @@ export default {
 						}
 						.play-icon {
 							display: none;
-							height: 16rpx;
-							min-width: 18rpx;
+							height: 46rpx;
+							margin: 10rpx 0;
+							text-align: center;
+							align-items: center;
 							overflow: hidden;
 							position: relative;
-							z-index: 2;
-							line {
-								width: 2rpx;
+							z-index: 1;
+							transform: rotate(180deg);
+							left: 10rpx;
+							top: 10rpx;
+							font-size: 64rpx;
+							.line {
+								width: 10rpx;
 								height: 100%;
-								margin-left: 2rpx;
+								margin-left: 4rpx;
 								background-color: $uni-bg-color;
-								animation: play 0.9s linear infinite alternate;
+								animate-delay: play 1.6s linear;
+								animation-iteration-count: infinite; //是否循环执行动画
+								animation-duration: 2.5s; //动画持续时间完成
 							}
 						}
 						.play-btn {
 							cursor: pointer;
 							position: relative;
-							margin: 0 auto;
 							align-items: center;
+							font-size: 64rpx;
 							color: #ffffff;
 							&:hover {
 								color: rgb(211, 213, 210);
 							}
 						}
 						.pause-btn {
+							font-size: 64rpx;
 							display: none;
 							cursor: pointer;
 							position: relative;
@@ -199,47 +214,39 @@ export default {
 							}
 						}
 					}
-				}
-				&.playing {
-					.index-container {
-						.pause-btn {
-							display: none;
-						}
-						.play-icon {
-							display: flex;
-						}
-						.play-btn {
-							display: none;
-						}
-					}
-				}
-				&:hover {
-					.index-container {
-						.play-btn {
-							display: block;
-						}
-					}
 					&.playing {
 						.index-container {
+							.play-icon {
+								display: flex;
+							}
 							.play-btn {
 								display: none;
 							}
-							.play-icon {
-								display: none;
-							}
 							.pause-btn {
-								display: block;
+								display: none;
 							}
 						}
 					}
-				}
-				.play-name {
-					height: auto;
-					color: #333;
-					margin-top: 8rpx;
-					font-size: 26rpx;
-					word-wrap: break-word;
-					word-break: normal;
+					&:hover {
+						.index-container {
+							.play-btn {
+								display: block;
+							}
+						}
+						&.playing {
+							.index-container {
+								.play-btn {
+									display: none;
+								}
+								.play-icon {
+									display: none;
+								}
+								.pause-btn {
+									display: block;
+								}
+							}
+						}
+					}
 				}
 			}
 		}
