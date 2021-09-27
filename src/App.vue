ca23fd41cf30295d7e1fdc056b3c4e83d165297e
Romantic <468264345@qq.com>
2021-09-27 22:25:53 +0800

    在app挂载iconfont.css

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,6 @@ export default {
 /*每个页面公共css */
 /* 注意要写在第一行，同时给style标签加入lang="scss"属性 */
 @import 'uview-ui/index.scss';
-@import '/static/css/style.css';
+@import './static/css/style.css';
 
 </style>
