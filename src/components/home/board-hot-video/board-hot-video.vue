0e75eb9efcc8961789be25a8043eafef880e243d
Romantic <468264345@qq.com>
2021-09-30 22:06:21 +0800

    热门视频

---
+++ src/components/home/board-hot-video/board-hot-video.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class="">123</view>
+</template>
+
+<script>
+/**
+ * author	bugdr
+ * time     2021-9-30 6:29:14 ?F10: PM?
+ * description 热门视频
+ */
+
+export default {
+	name: 'board-hot-video',
+	data() {
+		return {};
+	},
+	component: {},
+	mounted() {},
+	methods: {}
+};
+</script>
+
+<style lang="scss" scoped></style>
