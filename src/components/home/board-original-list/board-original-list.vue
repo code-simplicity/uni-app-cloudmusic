99db6000d205ededcf6122dd9551046879aa87ca
Romantic <468264345@qq.com>
2021-09-30 22:07:05 +0800

    原创榜

---
+++ src/components/home/board-original-list/board-original-list.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class="">789</view>
+</template>
+
+<script>
+/**
+ * author	bugdr
+ * time     2021-9-30 6:29:14 ?F10: PM?
+ * description 原创榜
+ */
+
+export default {
+	name: 'board-original-list',
+	data() {
+		return {};
+	},
+	component: {},
+	mounted() {},
+	methods: {}
+};
+</script>
+
+<style lang="scss" scoped></style>
