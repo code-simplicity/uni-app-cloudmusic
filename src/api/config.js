c98a203af656f059a70aea16e13dd2f7c4243955
Romantic <468264345@qq.com>
2021-09-30 22:04:40 +0800

    接口地址修改为自己服务器的

--- src/api/config.js
+++ src/api/config.js
@@ -1,8 +1,8 @@
 // 接口地址
 let api_base_url = ''
 if (process.env.NODE_ENV === 'development') {
-	// 开发地址
-	api_base_url = 'http://localhost:3000'
+	// 开发地址，直接使用服务器部署的地址
+	api_base_url = 'http://152.136.183.34:3000/'
 } else if (process.env.NODE_ENV === 'production') {
 	// 生产模式的地址
 	api_base_url = 'https://netease-cloud-music-api-zeta-murex.vercel.app/'
