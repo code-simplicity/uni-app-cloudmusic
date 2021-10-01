271d9bf787753f396b434ac4b815f7cea9f1e198
Romantic <468264345@qq.com>
2021-10-01 21:04:05 +0800

    推荐歌单

--- src/api/service/songlist.js
+++ src/api/service/songlist.js
@@ -4,3 +4,8 @@ import api from '../../common/luch-request.js'
  * 获取每日推荐歌单
  */
 export const getRecommendResource = () => api.get(`/recommend/resource`, {})
+
+/**
+ * 获取推荐歌单
+ */
+export const getPersonalized = (limit) => api.get(`personalized?limit=${limit}`, {})
