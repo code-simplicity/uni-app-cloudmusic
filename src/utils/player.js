4efbf3cbd13945c2b67f77eb54a92bc16492708a
Romantic <468264345@qq.com>
2021-10-01 21:09:30 +0800

    播放器

--- src/utils/player.js
+++ src/utils/player.js
@@ -1,20 +1,24 @@
 // 引入缓存的工具
 import {
 	saveHistory
-} from 'cache.js'
+} from './cache.js'
 
 // 播放器实例，全局调用，避免污染
-let player = {}
+// let player = {}
 
-// 创建全局唯一的播放实例
-function getPlayer() {
-	const innerAudioContext = uni.createInnerAudioContext();
-	innerAudioContext.autoplay = true;
-	return innerAudioContext
-}
+const player = uni.createInnerAudioContext()
+player.autoplay = true;
+
+
+// // 创建全局唯一的播放实例
+// function getPlayer() {
+// 	const innerAudioContext = uni.createInnerAudioContext();
+// 	innerAudioContext.autoplay = true;
+// 	return innerAudioContext
+// }
 
 // 播放器地址
-player.setUrl() = function(url) {
+player.setUrl = function(url) {
 	url = url || this.saveHistory().url
 	let player = getPlayer()
 	player.src = url
@@ -28,7 +32,7 @@ player.play = function() {
 	let player = getPlayer()
 	player.play(() => {
 		console.log('音乐开始播放')
-	}，(err) => {
+	}, (err) => {
 		console.log(err)
 	})
 	return
@@ -48,5 +52,7 @@ player.getDuration = function() {
 
 // 上一首
 player.prev = function(song) {
-	
+
 }
+
+export default player
