6ea9b88d46e00dc59dfa0af130d0e8c6970f6a8d
Romantic <468264345@qq.com>
2021-10-04 22:47:48 +0800

    用户信息模块

---
+++ src/components/user/user-info/user-info.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class="user-info"></view>
+</template>
+
+<script>
+/**
+ * author	bugdr
+ * time     2021-10-4 7:32:34 ?F10: PM?
+ * description
+ */
+
+export default {
+	name: 'user-info',
+	data() {
+		return {};
+	},
+	component: {},
+	mounted() {},
+	methods: {}
+};
+</script>
+
+<style lang="scss" scoped></style>
