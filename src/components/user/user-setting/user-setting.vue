c9fece13eee052a992a65184dd1ec01c0555cba7
Romantic <468264345@qq.com>
2021-10-05 20:17:02 +0800

    底部功能栏开发，进入用户设置界面的功能栏

---
+++ src/components/user/user-setting/user-setting.vue
@@ -0,0 +1,40 @@
+<template>
+	<view class="user-setting">
+		<view class="user-info-tool" @click="toUserSetting">
+			<u-icon name="grid-fill" size="50" color="#000000"></u-icon>
+		</view>
+	</view>
+</template>
+
+<script>
+/**
+ * author	bugdr
+ * time     2021-10-5 10:16:39 ?F10: AM?
+ * description
+ */
+
+export default {
+	name: 'user-setting',
+	data() {
+		return {};
+	},
+	component: {},
+	mounted() {},
+	methods: {
+		toUserSetting() {
+			this.$Router.push({
+				name: 'UserSetting'
+			});
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.user-setting {
+	.user-info-tool {
+		margin-right: 18rpx;
+		margin-top: 10rpx;
+	}
+}
+</style>
