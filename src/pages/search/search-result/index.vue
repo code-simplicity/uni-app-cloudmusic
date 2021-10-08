c876a87c5252e3c64b0e90a11a437805759d553c
Romantic <468264345@qq.com>
2021-10-08 08:53:56 +0800

    搜索结果页面

--- src/pages/search/search-result/index.vue
+++ src/pages/search/search-result/index.vue
@@ -2,19 +2,7 @@
 	<view class="search-result">
 		<view class="search-header">
 			<u-navbar>
-				<u-search
-					:placeholder="searchDefault"
-					v-model="searchKeyword"
-					:show-action="true"
-					action-text="搜索"
-					:focus="true"
-					:clearabled="true"
-					:animation="true"
-					margin="0 20rpx 0 0"
-					@search="search"
-					@custom="search"
-					@change="change"
-				></u-search>
+				<search-bar ref="searchBar" @search="onSearch" @custom="onSearch" @change="change"></search-bar>
 			</u-navbar>
 		</view>
 
@@ -29,9 +17,16 @@
 				swiperWidth="750"
 			></u-tabs-swiper>
 		</view>
-		<swiper :current="swiperCurrent" @transition="transition" @animationfinish="animationfinish">
+		<swiper
+			class="search-swiper"
+			:current="swiperCurrent"
+			@transition="transition"
+			@animationfinish="animationfinish"
+		>
 			<swiper-item class="swiper-item">
-				<scroll-view scroll-y style="height: 800rpx;width: 100%;" @scrolltolower="onreachBottom"></scroll-view>
+				<scroll-view scroll-y style="width: 100%; height: 100%;" @scrolltolower="onreachBottom">
+					<search-multiple ref="searchMultiple"></search-multiple>
+				</scroll-view>
 			</swiper-item>
 		</swiper>
 	</view>
@@ -93,13 +88,95 @@ export default {
 			swiperCurrent: 0
 		};
 	},
+
 	component: {},
-	mounted() {},
+
+	onLoad() {
+		// 放入这个生命周期中，当外部点击搜索之后，在直接显示出搜索结果
+		let searchKeyword = this.$Route.query.searchKeyword;
+		if (searchKeyword) {
+			this.searchKeyword = searchKeyword;
+		}
+	},
+
+	onReady() {
+		this.getCloudSearch(this.searchKeyword, this.swiperCurrent);
+	},
+
+	created() {},
+
+	mounted() {
+		this.$refs.searchBar.setKeyword(this.searchKeyword);
+	},
+
 	methods: {
+		// 值改变之后出现搜索建议的框
+		change() {},
+		// 执行搜索
+		onSearch(keyword) {
+			if (!keyword) {
+				return;
+			}
+			this.searchKeyword = keyword;
+			this.$refs.searchBar.setKeyword(keyword);
+			// 执行搜索，传入关键字以及tabs切换的值
+			this.getCloudSearch(keyword, this.swiperCurrent);
+		},
+
+		/**
+		 * 搜索
+		 * type: 搜索类型；取值意义 : 1: 单曲,
+		 * 10: 专辑, 100: 歌手,
+		 * 1000: 歌单, 1002: 用户,
+		 * 1004: MV, 1006: 歌词,
+		 *  1009: 电台, 1014: 视频, 1018:综合
+		 */
+		getCloudSearch(keyword, index) {
+			if (!keyword) {
+				return;
+			}
+			// 综合,传递输入框关键字给子组件
+			switch (index) {
+				case 0: {
+					this.$refs.searchMultiple.getSearchMultimatch(keyword);
+					break;
+				}
+				case 1: {
+					this.$refs.searchMultiple.getSearchMultimatch(keyword);
+					break;
+				}
+				case 2: {
+					this.$refs.searchMultiple.getSearchMultimatch(keyword);
+					break;
+				}
+				case 3: {
+					this.$refs.searchMultiple.getSearchMultimatch(keyword);
+					break;
+				}
+				case 4: {
+					this.$refs.searchMultiple.getSearchMultimatch(keyword);
+					break;
+				}
+				case 5: {
+					this.$refs.searchMultiple.getSearchMultimatch(keyword);
+					break;
+				}
+				case 6: {
+					this.$refs.searchMultiple.getSearchMultimatch(keyword);
+					break;
+				}
+				case 7: {
+					this.$refs.searchMultiple.getSearchMultimatch(keyword);
+					break;
+				}
+			}
+		},
+
 		// tabs通知swiper切换
 		tabChange(index) {
 			this.swiperCurrent = index;
 			console.log(index);
+			this.getCloudSearch(this.searchKeyword, index);
 		},
 
 		// swiper-item左右移动，通知tabs的滑块跟随移动
@@ -114,6 +191,9 @@ export default {
 			let current = e.detail.current;
 			this.$refs.uTabs.setFinishCurrent(current);
 			this.swiperCurrent = current;
+			if (this.currentTab !== current) {
+				this.getCloudSearch(this.searchKeyword, current);
+			}
 			this.currentTab = current;
 		}
 	}
@@ -123,12 +203,24 @@ export default {
 <style lang="scss" scoped>
 .search-result {
 	width: 100%;
+	height: 100%;
+	top: 0;
+	left: 0;
+	right: 0;
+	position: absolute;
+	display: flex;
+	flex-direction: column;
+	flex-wrap: nowrap;
+
 	.search-header {
 		width: 100%;
 	}
 	.search-wrap {
 		margin-top: 10rpx;
 		width: 100%;
+	}
+	.search-swiper {
+		flex: 1;
 	}
 }
 </style>
